7f9e5d98eb9104b92c8cc90d32987d5357736db1
d1mak3 <d1mak3@yandex.ru>
2020-11-21 03:18:08 +0300

    done

--- Queue.cs
+++ Queue.cs
@@ -10,26 +10,10 @@ namespace TMP_1lab
     {
         private int[] queue = new int[0];
 
-        public Queue(int[] _oldQueue, int _index)//Копирует очередь до определённого числа
+        public Queue()
         {
-            for (int i = 0; i < _index; ++i)
-            {
-                queue[i] = _oldQueue[i];
-            }
-        }
 
-        public Queue(int[] _oldQueue, int _index1, int _index2)//Копирует очердь с определённого числа до определённого числа
-        {
-            for (int i = _index1; i < _index2; ++i)
-            {
-                queue[i] = _oldQueue[i];
-            }
-        }
-
-        public Queue(int[] _copyQueue)
-        {
-            queue = _copyQueue;
-        }
+        } 
 
         public int QueueSize //Размер очереди
         {
@@ -39,7 +23,7 @@ namespace TMP_1lab
 
                 foreach (int o in queue)
                 {
-                    ++size;
+                    ++size;                    
                 }
                 return size;
             }
@@ -47,48 +31,47 @@ namespace TMP_1lab
 
         public void Push(int _newElement)//Добавляет число _newElement в конец очереди (в начало массива)
         {
-            int[] temp = new int[QueueSize];
-            for (int i = QueueSize; i >= 0; i--)
+            int size = QueueSize;
+            int[] temp = new int[size];
+            for (int i = 0; i < size; ++i)
             {
-                queue[i] = queue[i - 1];
+                temp[i] = queue[i];
             }
+
+            queue = new int[size + 1];
             queue[0] = _newElement;
+
+            for (int i = 1; i < size + 1; ++i)
+			{
+                queue[i] = temp[i - 1];
+			}            
         }
 
         public int Pop()//Возвращает первый элемент в очереди (последний элемент массива) и при этом удаляет его из этой очереди
         {
-            int rez = queue[QueueSize];
-            for (int i = 0; i < QueueSize; ++i)
-            {
+            int size = QueueSize;
+            int rez = queue[size - 1]; // QueueSize - 1 потому что в массиве элементы считаются с 0
+            int[] temp = new int[size];
+            for (int i = 0; i < size; ++i)
+			{
+                temp[i] = queue[i];
+			}
+            queue = new int[size - 1];
 
-            }
+            for (int i = 0; i < size - 1; ++i)
+			{
+                queue[i] = temp[i];
+			}
+            
             return rez;
         }
 
         public int Back()//Возвращает первый элемент в очереди (последний в массиве) и НЕ удаляет его из очереди
         {
-            int rez = queue[QueueSize];
+            int rez = queue[QueueSize - 1]; // QueueSize - 1 потому что в массиве элементы считаются с 0
             return rez;
         }
-
-        public void Sort(int k, int[] queue)
-        {
-            var count = new int[k + 1]; ;
-            for (int i = 0; i < QueueSize; i++)
-            {
-                count[queue[i]++];
-            }
-
-            int index = 0;
-            for (int i = 0; i < count.Length; i++)
-            {
-                for (int j = 0; j < count[i]; j++)
-                {
-                    queue[index] = i;
-                    index++;
-                }
-            }
-        }
+        
         public void Print()
         {
             foreach (int item in queue)
